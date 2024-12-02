209df8f296c7541a09682a7e461d38f7fb54dd56
mu-nss <msj20161282@gmail.com>
2024-12-02 18:14:09 +0900

    connected jdbc

--- src/test/java/zerobase/weather/WeatherApplicationTests.java
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -3,26 +3,24 @@ package zerobase.weather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class WeatherApplicationTests {
 
-	@Test
-	void equalTest () {
-		assertEquals(1, 1);
-	}
-
+//	@Test
+//	void equalTest () {
+//		assertEquals(1, 1);
+//	}
 
 	@Test
 	void nullTest () {
 		assertNull(null);
 	}
 
-	@Test
-	void trueTest () {
-		assertTrue(1 == 1);
-	}
+//	@Test
+//	void trueTest () {
+//		assertTrue(1 == 1);
+//	}
 
 }
